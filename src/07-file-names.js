a43c68725b8f1d10153bcb2f2338032b44e3b85d
AndrewPerepelkin <avp.tep@rambler.ru>
2021-04-11 00:30:22 +0300

    feat: add solution 07

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -1,20 +1,34 @@
 /**
- * There's a list of file, since two files cannot have equal names,
- * the one which comes later will have a suffix (k),
- * where k is the smallest integer such that the found name is not used yet.
- *
- * Return an array of names that will be given to the files.
- *
- * @param {Array} names
- * @return {Array}
- *
- * @example
- * For input ["file", "file", "image", "file(1)", "file"],
- * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
- *
- */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+* There's a list of file, since two files cannot have equal names,
+* the one which comes later will have a suffix (k),
+* where k is the smallest integer such that the found name is not used yet.
+*
+* Return an array of names that will be given to the files.
+*
+* @param {Array} names
+* @return {Array}
+*
+* @example
+* For input ["file", "file", "image", "file(1)", "file"],
+* the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
+*
+*/
+function renameFiles(names) {
+  const renames = [];
+  const obj = {};
+  names.forEach((name) => {
+    if (renames.includes(name)) {
+      if (!(name in obj)) {
+        obj[name] = 1;
+      }
+      renames.push(`${name}(${obj[name]})`);
+      obj[name]++;
+    } else {
+      obj[name] = 1;
+      renames.push(name);
+    }
+  });
+  return renames;
 }
 
 module.exports = renameFiles;
