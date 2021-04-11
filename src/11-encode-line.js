7661e4357a917c8b680fbc60b8a9b77f4a113f5c
AndrewPerepelkin <avp.tep@rambler.ru>
2021-04-11 03:08:20 +0300

    feat: add solution 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const code = [];
+  const obj = {};
+  str.split('').forEach((c, i, arr) => {
+    if (c === arr[i + 1]) {
+      if (c in obj) {
+        obj[c]++;
+      } else {
+        obj[c] = 2;
+      }
+    } else if (c in obj) {
+      code.push(`${obj[c]}${c}`);
+    } else {
+      code.push(c);
+    }
+  });
+  return code.join('');
 }
 
 module.exports = encodeLine;
